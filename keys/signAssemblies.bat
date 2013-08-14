0bbcf014684a18f1b6316c9055a1a42fbbb92ce0
Patrik Lindström <patrik.lindstrom@lcube.se>
2013-08-14 15:11:42 +0200

    changed signed batfile

--- keys/signAssemblies.bat
+++ keys/signAssemblies.bat
@@ -1,5 +1,7 @@
 
-Rem sign with certificate
-c:\Program Files\Microsoft SDKs\Windows\v7.1A\Bin>signtool sign /f "C:\Users\xxlindtp\Documents\GitHub\CgiExperimentF5\keys\CGIRuler2.pfx" /p niltap1 /v :\Users\xxlindtp\Documents\GitHub\CgiExperimentF5\F5\bin\Debug\F5.exe" 
+@echo sign with certificate
+"c:\Program Files\Microsoft SDKs\Windows\v7.1A\Bin\signtool" sign /f "C:\Users\xxlindtp\Documents\GitHub\CgiExperimentF5\keys\CGIRuler2.pfx" /p niltap1 /v :\Users\xxlindtp\Documents\GitHub\CgiExperimentF5\F5\bin\Debug\F5.exe" 
 
-c:\Program Files\Microsoft SDKs\Windows\v7.1A\Bin>signtool sign /f "C:\Users\xxlindtp\Documents\GitHub\CgiExperimentF5\keys\CGIRuler2.pfx" /p niltap1 /v "C:\Users\xxlindtp\Documents\GitHub\CgiExperimentF5\F5\bin\Debug\Essential.Diagnostics.dll"
+"c:\Program Files\Microsoft SDKs\Windows\v7.1A\Bin\signtool" sign /f "C:\Users\xxlindtp\Documents\GitHub\CgiExperimentF5\keys\CGIRuler2.pfx" /p niltap1 /v "C:\Users\xxlindtp\Documents\GitHub\CgiExperimentF5\F5\bin\Debug\Essential.Diagnostics.dll"
+
+pause
