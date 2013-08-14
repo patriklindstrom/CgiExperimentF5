cb18802bd3608f8d14bb6a058499b681ddb97c4a
Patrik Lindström <patrik.lindstrom@lcube.se>
2013-08-14 15:46:59 +0200

    logging works now
    I put the loggin folder in : C:\inetpub\cgi-bin\Logs and changed the
    rights for the folder so it could be written to.

--- keys/signAssemblies.bat
+++ keys/signAssemblies.bat
@@ -1,7 +1,9 @@
-
+@echo Has to be run as admin
 @echo sign with certificate
 "c:\Program Files\Microsoft SDKs\Windows\v7.1A\Bin\signtool" sign /f "C:\Users\xxlindtp\Documents\GitHub\CgiExperimentF5\keys\CGIRuler2.pfx" /p niltap1 /v :\Users\xxlindtp\Documents\GitHub\CgiExperimentF5\F5\bin\Debug\F5.exe" 
 
 "c:\Program Files\Microsoft SDKs\Windows\v7.1A\Bin\signtool" sign /f "C:\Users\xxlindtp\Documents\GitHub\CgiExperimentF5\keys\CGIRuler2.pfx" /p niltap1 /v "C:\Users\xxlindtp\Documents\GitHub\CgiExperimentF5\F5\bin\Debug\Essential.Diagnostics.dll"
+@echo copy dll and config from debug
+robocopy "C:\Users\xxlindtp\Documents\GitHub\CgiExperimentF5\F5\bin\Debug"  "C:\inetpub\cgi-bin" Essential.Diagnostics.dll F5.exe F5.exe.config
 
 pause
